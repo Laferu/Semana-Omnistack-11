4ed0d5e1ed5843eb2766c44a44c40bf7ecae877a
Laferu <rp.araujo2@gmail.com>
2020-03-26 11:48:36 -0300

    Retornando e enviado dados para o Back-End

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiLogIn } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+
+import api from '../../services/api'
 
 import './styles.css'
 
@@ -8,15 +10,38 @@ import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 const Logon = () => {
+
+  const [id, setId] = useState('')
+  const history = useHistory()
+
+  const handleLogin = async e => {
+    e.preventDefault()
+
+    try {
+      const response = await api.post('sessions', { id })
+
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
+
+      history.push('/profile')
+    } catch (error) {
+      alert('Falha no Login. Tente novamente!')
+    }
+  }
+
   return (
     <div className='logon-container'>
       <section className='form'>
         <img src={logoImg} alt='Be The Hero' />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder='Sua ID' />
+          <input
+            placeholder='Sua ID'
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className='button' type='submit'>Entrar</button>
 
           <Link className='back-link' to='/register'>
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,20 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiPower, FiTrash2 } from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
 import logoImg from '../../assets/logo.svg'
 
 const Profile = () => {
+  const ongId = localStorage.getItem('ongId')
+  const ongName = localStorage.getItem('ongName')
+
+  const history = useHistory()
+
+  const [incidents, setIncidents] = useState([])
+
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId
+      }
+    }).then(response => {
+      setIncidents(response.data)
+    })
+  }, [ongId])
+
+  const handleDeleteIncident = async id => {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      })
+
+      setIncidents(incidents.filter(e => e.id !== id))
+    } catch (error) {
+      alert('Erro ao deletar caso, tente novamente.')
+    }
+  }
+
+  const handleLogout = () => {
+    localStorage.clear()
+
+    history.push('/')
+  }
+
   return (
     <div className='profile-container'>
       <header>
         <img src={logoImg} alt='Be The Hero' />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link className='button' to='/incidents/new'>Cadastrar novo caso</Link>
-        <button type='button'>
+        <button type='button' onClick={handleLogout}>
           <FiPower size={18} color='#e02041' />
         </button>
       </header>
@@ -22,18 +61,18 @@ const Profile = () => {
       <h1>Casos cadastrados</h1>
       <ul>
         {
-          [1, 2, 3].map((e, index) => (
-            <li key={index}>
+          incidents.map(e => (
+            <li key={e.id}>
               <strong>CASO:</strong>
-              <p>Caso teste</p>
+              <p>{e.title}</p>
 
               <strong>DESCRIÇÃO:</strong>
-              <p>Descrição teste</p>
+              <p>{e.description}</p>
 
               <strong>VALOR:</strong>
-              <p>R$ 120,00</p>
+              <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(e.value)}</p>
 
-              <button type='button'>
+              <button onClick={() => handleDeleteIncident(e.id)} type='button'>
                 <FiTrash2 size={20} color='#a8a8b3' />
               </button>
             </li>
